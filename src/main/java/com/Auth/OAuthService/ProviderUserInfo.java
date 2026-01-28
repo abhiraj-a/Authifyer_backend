403a3fba769c298a24295b6af1c232008516f3c4
abhiraj-a <abhirajp0608@gmail.com>
2026-01-28 22:10:44 +0530

    Delete src/main/java/com/Auth/OAuthService directory
    delete redundant

--- src/main/java/com/Auth/OAuthService/ProviderUserInfo.java
+++
@@ -1,9 +0,0 @@
-package com.Auth.OAuthService;
-
-import lombok.Builder;
-
-@Builder
-public class ProviderUserInfo {
-    private String providerUserId; // IMMUTABLE (sub / id)
-    private String email;
-}
