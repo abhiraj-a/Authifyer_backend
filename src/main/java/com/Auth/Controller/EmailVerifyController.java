2214ff7eb015cb2b7537250fb546320cc0adf9b4
abhiraj-a <abhirajp0608@gmail.com>
2026-02-22 15:02:58 +0530

    email verification logic changed vt attempts added ,method now in email service

--- src/main/java/com/Auth/Controller/EmailVerifyController.java
+++ src/main/java/com/Auth/Controller/EmailVerifyController.java
@@ -10,10 +10,12 @@ import com.Auth.Exception.VerifictionTokenExpired;
 import com.Auth.Repo.GlobalUserRepo;
 import com.Auth.Repo.ProjectUserRepo;
 import com.Auth.Repo.VerificationTokenRepo;
+import com.Auth.Service.EmailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import java.time.Instant;
 
@@ -25,46 +27,12 @@ public class EmailVerifyController {
     private final VerificationTokenRepo verificationTokenRepo;
     private final GlobalUserRepo globalUserRepo;
     private final ProjectUserRepo projectUserRepo;
+    private final EmailService emailService;
 
     @PostMapping
     public ResponseEntity<?> verifyEmail(@RequestBody VerifyEmailRequest request){
-        VerificationToken vt= verificationTokenRepo.findByVerificationToken(request.getToken())
-                .orElseThrow(VerificationTokenNotFound::new);
-        if (vt.getExpiresAt().isBefore(Instant.now())) {
-            verificationTokenRepo.delete(vt);
-            throw new VerifictionTokenExpired();
-        }
-        if (vt.getAttempts() >= 5) {
-            verificationTokenRepo.delete(vt);
-            throw new ApiException("Max verification attempts reached. Please request a new token.", HttpStatus.BAD_REQUEST);
-        }
-        if (!vt.getVerificationToken().equals(request.getToken())) {
-            vt.setAttempts(vt.getAttempts() + 1);
-            verificationTokenRepo.save(vt);
-            throw new ApiException("Invalid verification code.", HttpStatus.BAD_REQUEST);
-        }
-
-        String subjectId= vt.getSubjectId();
-        if(!subjectId.equals(request.getSubjectId())){
-            log.warn("Subject id did not match");
-            throw new ApiException("Invalid" , HttpStatus.BAD_REQUEST);
-        }
-        if(subjectId.startsWith("glob_usr_")){
-            GlobalUser user = globalUserRepo.findBySubjectId(subjectId).orElseThrow(UserNotFoundException::new);
-            user.setEmailVerified(true);
-            globalUserRepo.save(user);
-            log.warn("email verified " + user.getEmail());
-            return ResponseEntity.ok().build();
-        } else if (subjectId.startsWith("auth_usr_")) {
-            ProjectUser user = projectUserRepo.findByAuthifyerId(subjectId).orElseThrow(UserNotFoundException::new);
-            user.setEmailVerified(true);
-            projectUserRepo.save(user);
-            log.warn("email verified " + user.getEmail());
-            return ResponseEntity.ok().build();
-        }
-        else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
+        emailService.verifyEmail(request);
+        return ResponseEntity.ok().build();
     }
 
 }
--- src/main/java/com/Auth/Service/EmailService.java
+++ src/main/java/com/Auth/Service/EmailService.java
@@ -1,6 +1,15 @@
 package com.Auth.Service;
 
+import com.Auth.DTO.VerifyEmailRequest;
+import com.Auth.Entity.GlobalUser;
+import com.Auth.Entity.ProjectUser;
 import com.Auth.Entity.VerificationToken;
+import com.Auth.Exception.ApiException;
+import com.Auth.Exception.UserNotFoundException;
+import com.Auth.Exception.VerificationTokenNotFound;
+import com.Auth.Exception.VerifictionTokenExpired;
+import com.Auth.Repo.GlobalUserRepo;
+import com.Auth.Repo.ProjectUserRepo;
 import com.Auth.Repo.VerificationTokenRepo;
 import com.Auth.Util.VerifyUser;
 import jakarta.mail.MessagingException;
@@ -8,10 +17,7 @@ import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -28,6 +34,8 @@ import java.util.UUID;
 @Slf4j
 public class EmailService {
 
+    private final GlobalUserRepo globalUserRepo;
+    private final ProjectUserRepo projectUserRepo;
 private final VerificationTokenRepo verificationTokenRepo;
     private final RestTemplate restTemplate;
 
@@ -108,5 +116,42 @@ private final VerificationTokenRepo verificationTokenRepo;
                 user.getName(),
                 token
         );
+    }
+
+    @Transactional
+    public void verifyEmail(VerifyEmailRequest request) {
+        VerificationToken vt= verificationTokenRepo.findByVerificationToken(request.getToken())
+                .orElseThrow(VerificationTokenNotFound::new);
+        if (vt.getExpiresAt().isBefore(Instant.now())) {
+            verificationTokenRepo.delete(vt);
+            throw new VerifictionTokenExpired();
+        }
+        if (vt.getAttempts() >= 5) {
+            verificationTokenRepo.delete(vt);
+            throw new ApiException("Max verification attempts reached. Please request a new token.", HttpStatus.BAD_REQUEST);
+        }
+        if (!vt.getVerificationToken().equals(request.getToken())) {
+            vt.setAttempts(vt.getAttempts() + 1);
+            verificationTokenRepo.save(vt);
+            throw new ApiException("Invalid verification code.", HttpStatus.BAD_REQUEST);
+        }
+
+        String subjectId= vt.getSubjectId();
+        if(!subjectId.equals(request.getSubjectId())){
+            log.warn("Subject id did not match");
+            throw new ApiException("Invalid" , HttpStatus.BAD_REQUEST);
+        }
+        if(subjectId.startsWith("glob_usr_")){
+            GlobalUser user = globalUserRepo.findBySubjectId(subjectId).orElseThrow(UserNotFoundException::new);
+            user.setEmailVerified(true);
+            globalUserRepo.save(user);
+            log.warn("email verified " + user.getEmail());
+        } else if (subjectId.startsWith("auth_usr_")) {
+            ProjectUser user = projectUserRepo.findByAuthifyerId(subjectId).orElseThrow(UserNotFoundException::new);
+            user.setEmailVerified(true);
+            projectUserRepo.save(user);
+            log.warn("email verified " + user.getEmail());
+        }
+
     }
 }
